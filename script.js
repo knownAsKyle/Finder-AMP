cc319821238922de681cad78518406fd7080a29f
Kyle <Kyle.Uhan@gmail.com>
2016-04-21 16:15:27 -0500

    updates

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 var _Searcher_ = _Searcher_ || {};
+function _cb_findItemsByKeywords(root) {
+	_Searcher_._cb_findItemsByKeywords(root);
+}
+(function() {
+    _Searcher_.elements = {};
+    _Searcher_.elements.inputSearch = document.getElementById("input-search");
+    _Searcher_.elements.resultsWrapper = document.getElementById("results-wrapper");
+    _Searcher_.elements.ebayScript = document.getElementById("ebay_search_tag");
+    _Searcher_.elements.ebayCheckbox = document.getElementById("ebay-checkbox");
+    _Searcher_.elements.craigsListCheckbox = document.getElementById("craigs-list-checkbox");
+    _Searcher_.elements.amazonCheckbox = document.getElementById("amazon-checkbox");
+    _Searcher_.elements.etsyCheckbox = document.getElementById("etsy-checkbox");
+})();
 (function() {
     _Searcher_.consts = {};
     _Searcher_.consts.ebay = {};
     _Searcher_.consts.ebay.KEYWORDS = "findItemsByKeywords";
-    _Searcher_.consts.ebay.RESULT_AMOUNT = "15";
+    _Searcher_.consts.ebay.RESULT_AMOUNT = "200";
     _Searcher_.consts.ebay.SERVICE_VERSION = "1.0.0";
     _Searcher_.consts.ebay.endPoint = "http://svcs.ebay.com/services/search/FindingService/v1";
     _Searcher_.consts.ebay.operation = "?OPERATION-NAME=";
@@ -17,21 +30,24 @@ var _Searcher_ = _Searcher_ || {};
     _Searcher_.consts.ebay.pagination = "&paginationInput.entriesPerPage=";
 })();
 (function() {
-    var search = document.getElementById("input-search");
-    var resultsWrapper = document.getElementById("results-wrapper");
+    var search = _Searcher_.elements.inputSearch;
+    var resultsWrapper = _Searcher_.elements.resultsWrapper;
+    search.disabled = false;
     search.addEventListener("keyup", handleSearchType);
-    var beginSearch = null;
+    var beginEbaySearch = null;
 
     function handleSearchType(e) {
-        clearTimeout(beginSearch);
+        clearTimeout(beginEbaySearch);
         if (e.target.value && e.target.value !== "" && e.target.value.length > 0) {
             resultsWrapper.innerText = "Searching ...";
-            beginSearch = setTimeout(activateSearch, 1500);
+            if (_Searcher_.elements.ebayCheckbox.checked) {
+                beginEbaySearch = setTimeout(activateEbaySearch, 1200);
+            }
         } else {
             resultsWrapper.innerText = "";
         }
 
-        function activateSearch() {
+        function activateEbaySearch() {
             var s = document.createElement('script');
             s.id = "ebay_search_tag";
             var url = [];
@@ -57,10 +73,9 @@ var _Searcher_ = _Searcher_ || {};
 })();
 (function() {
     _Searcher_._cb_findItemsByKeywords = ebayKeyWordSearch;
-    var resultsWrapper = document.getElementById("results-wrapper");
+    var resultsWrapper = _Searcher_.elements.resultsWrapper;
 
     function ebayKeyWordSearch(root) {
-        console.dir(root);
         removeScriptTag();
         buildResults(root);
     }
@@ -78,7 +93,7 @@ var _Searcher_ = _Searcher_ || {};
     }
 
     function removeScriptTag() {
-        var script = document.getElementById("ebay_search_tag");
+        var script = _Searcher_.elements.ebayScript;
         if (script) {
             script.parentNode.removeChild(script);
         }
