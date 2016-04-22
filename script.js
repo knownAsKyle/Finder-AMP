80a1ba68541faacce70aa850df62dad21670548e
Kyle <Kyle.Uhan@gmail.com>
2016-04-22 10:25:33 -0500

    updates

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 var _Searcher_ = _Searcher_ || {};
-function _cb_findItemsByKeywords(root) {
-	_Searcher_._cb_findItemsByKeywords(root);
-}
 (function() {
     _Searcher_.elements = {};
     _Searcher_.elements.inputSearch = document.getElementById("input-search");
     _Searcher_.elements.resultsWrapper = document.getElementById("results-wrapper");
-    _Searcher_.elements.ebayScript = document.getElementById("ebay_search_tag");
+    _Searcher_.elements.ebayScript = function() {
+        return document.getElementById("ebay_search_tag");
+    };
     _Searcher_.elements.ebayCheckbox = document.getElementById("ebay-checkbox");
     _Searcher_.elements.craigsListCheckbox = document.getElementById("craigs-list-checkbox");
     _Searcher_.elements.amazonCheckbox = document.getElementById("amazon-checkbox");
@@ -16,7 +15,7 @@ function _cb_findItemsByKeywords(root) {
     _Searcher_.consts = {};
     _Searcher_.consts.ebay = {};
     _Searcher_.consts.ebay.KEYWORDS = "findItemsByKeywords";
-    _Searcher_.consts.ebay.RESULT_AMOUNT = "200";
+    _Searcher_.consts.ebay.RESULT_AMOUNT = "100";
     _Searcher_.consts.ebay.SERVICE_VERSION = "1.0.0";
     _Searcher_.consts.ebay.endPoint = "http://svcs.ebay.com/services/search/FindingService/v1";
     _Searcher_.consts.ebay.operation = "?OPERATION-NAME=";
@@ -24,7 +23,7 @@ function _cb_findItemsByKeywords(root) {
     _Searcher_.consts.ebay.security = "&SECURITY-APPNAME=KyleUhan-GetInfo-PRD-838ccaf50-1c06a23a";
     _Searcher_.consts.ebay.id = "&GLOBAL-ID=EBAY-US";
     _Searcher_.consts.ebay.responseType = "&RESPONSE-DATA-FORMAT=JSON";
-    _Searcher_.consts.ebay.callback = "&callback=_cb_findItemsByKeywords";
+    _Searcher_.consts.ebay.callback = "&callback=_Searcher_._cb_findItemsByKeywords";
     _Searcher_.consts.ebay.rest = "&REST-PAYLOAD";
     _Searcher_.consts.ebay.keyWordStarter = "&keywords=";
     _Searcher_.consts.ebay.pagination = "&paginationInput.entriesPerPage=";
@@ -41,39 +40,41 @@ function _cb_findItemsByKeywords(root) {
         if (e.target.value && e.target.value !== "" && e.target.value.length > 0) {
             resultsWrapper.innerText = "Searching ...";
             if (_Searcher_.elements.ebayCheckbox.checked) {
-                beginEbaySearch = setTimeout(activateEbaySearch, 1200);
+                beginEbaySearch = setTimeout(_Searcher_.activateEbaySearch, 1000, e.target.value);
             }
         } else {
             resultsWrapper.innerText = "";
         }
-
-        function activateEbaySearch() {
-            var s = document.createElement('script');
-            s.id = "ebay_search_tag";
-            var url = [];
-            url.push(_Searcher_.consts.ebay.endPoint);
-            url.push(_Searcher_.consts.ebay.operation);
-            url.push(_Searcher_.consts.ebay.KEYWORDS);
-            url.push(_Searcher_.consts.ebay.version);
-            url.push(_Searcher_.consts.ebay.SERVICE_VERSION);
-            url.push(_Searcher_.consts.ebay.security);
-            url.push(_Searcher_.consts.ebay.id);
-            url.push(_Searcher_.consts.ebay.responseType);
-            url.push(_Searcher_.consts.ebay.callback);
-            url.push(_Searcher_.consts.ebay.rest);
-            url.push(_Searcher_.consts.ebay.keyWordStarter);
-            url.push(e.target.value);
-            url.push(_Searcher_.consts.ebay.pagination);
-            url.push(_Searcher_.consts.ebay.RESULT_AMOUNT);
-            s.src = url.join("");
-            s.async = true;
-            document.body.appendChild(s);
-        }
     }
 })();
-(function() {
+(function ebaySeach() {
+    _Searcher_.ebaySearchResults = [];
+    _Searcher_.activateEbaySearch = activateEbaySearch;
     _Searcher_._cb_findItemsByKeywords = ebayKeyWordSearch;
     var resultsWrapper = _Searcher_.elements.resultsWrapper;
+
+    function activateEbaySearch(val) {
+        var s = document.createElement('script');
+        s.id = "ebay_search_tag";
+        var url = [];
+        url.push(_Searcher_.consts.ebay.endPoint);
+        url.push(_Searcher_.consts.ebay.operation);
+        url.push(_Searcher_.consts.ebay.KEYWORDS);
+        url.push(_Searcher_.consts.ebay.version);
+        url.push(_Searcher_.consts.ebay.SERVICE_VERSION);
+        url.push(_Searcher_.consts.ebay.security);
+        url.push(_Searcher_.consts.ebay.id);
+        url.push(_Searcher_.consts.ebay.responseType);
+        url.push(_Searcher_.consts.ebay.callback);
+        url.push(_Searcher_.consts.ebay.rest);
+        url.push(_Searcher_.consts.ebay.keyWordStarter);
+        url.push(val);
+        url.push(_Searcher_.consts.ebay.pagination);
+        url.push(_Searcher_.consts.ebay.RESULT_AMOUNT);
+        s.src = url.join("");
+        s.async = true;
+        document.body.appendChild(s);
+    }
 
     function ebayKeyWordSearch(root) {
         removeScriptTag();
@@ -93,34 +94,43 @@ function _cb_findItemsByKeywords(root) {
     }
 
     function removeScriptTag() {
-        var script = _Searcher_.elements.ebayScript;
+        var script = _Searcher_.elements.ebayScript();
         if (script) {
             script.parentNode.removeChild(script);
         }
     }
 })();
-(function() {
+(function addEbayData() {
     _Searcher_.buildDisplayListItem = buildDisplayListItem;
 
     function buildDisplayListItem(data) {
+        data = formatEbayData(data);
         var wrapper = document.createElement("div");
         var link = document.createElement("a");
         var img = document.createElement("img");
         var title = document.createElement("div");
-        var priceInfo = data.sellingStatus[0].sellingState[0] === "Active" ? data.sellingStatus[0].currentPrice[0].__value__ : "SOLD";
         wrapper.className = "list-item-wrapper";
-        link.href = data.viewItemURL[0];
-        link.title = data.viewItemURL[0];
+        link.href = data.link;
+        link.title = data.link;
         link.target = "_blank";
-        img.src = data.galleryURL[0];
-        img.alt = data.title[0];
-        img.title = data.title[0];
+        img.src = data.image;
+        img.alt = data.title;
+        img.title = data.title;
         img.height = "50";
         img.width = "50";
-        title.appendChild(document.createTextNode("(ebay) $" + priceInfo + " - " + data.title[0]));
+        title.appendChild(document.createTextNode("(ebay) $" + data.price + " - " + data.title));
         link.appendChild(img);
         link.appendChild(title);
         wrapper.appendChild(link);
         return wrapper;
+    }
+
+    function formatEbayData(data) {
+        var obj = {};
+        obj.price = data.sellingStatus[0].sellingState[0] === "Active" ? data.sellingStatus[0].currentPrice[0].__value__ : "SOLD";
+        obj.link = data.viewItemURL[0];
+        obj.image = data.galleryURL[0];
+        obj.title = data.title[0];
+        return obj;
     }
 })();
